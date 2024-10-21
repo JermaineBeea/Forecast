91323f4a91545bc3dc6c2b52ee5499c818f56fce
JermaineBeea <jermainebeea@gmail.com>
2024-10-21 17:39:55 +0200

    commit

--- Trade_of_EUR_USD/EURUSD.py
+++ Trade_of_EUR_USD/EURUSD.py
@@ -7,32 +7,32 @@
     "forecast_period_in_hrs": 492,
     "price_spread": 0.00042,
     "ZAR_to_USD": 0.05685,
-    "rate_opening": 1.08272,
+    "rate_opening": 1.08284,
     "initial_investment": 1000,
     "immediate_risk_factor": -0.00039,
-    "sample_immediate_risk": -0.3881,
+    "sample_immediate_risk": -0.38806,
     "forecast_closing_rates": [
-        0.84814,
-        0.97621,
-        1.10429,
-        1.17713,
-        1.24996
+        0.84844,
+        0.97647,
+        1.10451,
+        1.17732,
+        1.25013
     ],
     "profit_distribution_factors": [
-        -0.21696,
-        -0.09872,
-        0.01953,
-        0.08677,
-        0.15402
+        -0.21677,
+        -0.09858,
+        0.01962,
+        0.08683,
+        0.15405
     ],
     "sample_profit_distribution": [
-        -217.15339,
-        -98.8041,
-        19.5452,
-        86.84926,
-        154.15333
+        -216.95968,
+        -98.66254,
+        19.6346,
+        86.90901,
+        154.18343
     ],
-    "expected_closing_rate": 1.10429,
-    "expected_sample_profit": 19.5452,
-    "sample_max_possible_loss": -217.15339
+    "expected_closing_rate": 1.10451,
+    "expected_sample_profit": 19.6346,
+    "sample_max_possible_loss": -216.95968
 }
