e3a26f2293662a3002ee3766e1c13d98dbb66486
JermaineBeea <jermainebeea@gmail.com>
2024-10-22 17:10:58 +0200

    commit

---
+++ Trade_of_IBIT_USD/IBITUSD.py
@@ -0,0 +1,28 @@
+rate_given = "IBITUSD"
+time_of_trade = '2024-10-22 16:58:44'
+trade_action = "BUY USDIBIT"
+currency_investment = "USD"
+currency_profit = "ZAR"
+
+FUNCTION_FORECASTtion = "forecastData"
+start_date = 'None'
+end_date = 'None'
+period = '1d'
+interval = '1m'
+forecast_size = 29.66667
+price_spread = 0.05
+borrowing_fee = 0
+USD_to_USD = 1
+ZAR_to_USD = 0.05675
+initial_investment = 1000
+immediate_risk_factor = -0.0013
+sample_immediate_risk = -22.86827
+
+forecast_closing_rates = [36.71805, 37.96687, 39.21568, 40.04037, 40.86506]
+distr_profit_factor = [-0.0469, -0.01449, 0.01793, 0.03933, 0.06074]
+sample_profit_distribution = [-826.43493, -255.2704, 315.89414, 693.07827, 1070.26239]
+
+RATE_OPENING = 38.475
+EXPECTED_CLOSING_RATE = 39.21568
+expected_sample_profit = 315.89414
+sample_max_possible_loss = -826.43493
