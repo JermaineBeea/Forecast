39e060be0c62576cf3aac36054a10935693cf944
JermaineBeea <jermainebeea@gmail.com>
2024-10-22 17:40:49 +0200

    commit

--- Main_trade.py
+++ Main_trade.py
@@ -42,11 +42,11 @@ save_to_json = False
 
 # 3. Constants: Trading settings
 current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-sell_unit = 'IBIT'
-buy_unit = 'USD'
-currency_investment = 'USD'
+sell_unit = 'EUR'
+buy_unit = 'ZAR'
+currency_investment = sell_unit
 profit_currency = 'ZAR'
-price_spread = 0.05
+price_spread = 0.0252
 borrowing_fee = 0
 initial_investment = 1000 
 
@@ -179,131 +179,7 @@ output_variables = {
 }
 
 for key, val in param_period.items():
-    output_variables[key] = f"'{val}'"# 1. Import necessary modules and functions
-import os
-import time
-import json
-import pickle
-from functools import partial
-import pandas as pd
-import numpy as np
-from Modules.Forecast import forecastData
-from Modules.Forex import exchangeRate, get_conversion_rate, get_asset_price
-
-def format_value(value):
-    """Formats the value based on rounding and scientific notation settings."""
-    if isinstance(value, (int, float)):
-        if should_round:
-            value = round(value, rounding_precision)
-        if use_scientific_notation:
-            return f'{value:e}'
-    return value
-
-# Set display options for pandas and numpy
-np.set_printoptions(threshold=np.inf)
-pd.set_option('display.max_rows', None)
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_seq_items', None)
-
-# 2. Configuration: Data handling and formatting options
-fetch_external_data = True
-save_external_data = True
-use_binary_format = False 
-use_json_format = False
-use_csv_format = True
-
-# Configuration for data formatting
-should_round = True
-rounding_precision = 5
-use_scientific_notation = False
-
-# Configuration for writing variables
-allow_file_overwrite = True
-save_to_json = False
-
-
-# 3. Constants: Trading settings
-ASSET_TRADING = True
-current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-sell_unit = 'IBIT'
-buy_unit = 'USD' if not ASSET_TRADING else 'USD'
-currency_investment = 'USD' if not ASSET_TRADING else 'IBIT'
-profit_currency = 'ZAR'
-price_spread = 0.05
-borrowing_fee = 0
-
-# 4. File paths and parameters
-local_data_path = r'/home/wtc/Documents/RepositoryAccounts/Personal_GitHUb/Forecast/Trade Data/EURAUD.ifx.csv'
-trade_folder_path = f'Trade_of_{sell_unit}_{buy_unit}/'
-variable_file_name = f'{trade_folder_path}{sell_unit}{buy_unit}.py'
-external_data_file_name = f'{sell_unit}{buy_unit}_data'
-
-param_period = {
-'start_date': None,
-'end_date': None,
-'period': '1d',
-'interval': '1m'
-}
-field = 'Close'
-
-# Ensure trade folder exists
-os.makedirs(os.path.dirname(trade_folder_path), exist_ok=True) if os.path.dirname(trade_folder_path) else None
-
-# 5. Data Loading: Fetch or load data for trading
-if fetch_external_data:
-    # Fetch asset or conversion rate data
-
-    raw_data = (
-        partial(get_asset_price,ticker_symbol = sell_unit, **param_period)()
-        if ASSET_TRADING else 
-        partial(get_conversion_rate, sell_unit, buy_unit, **param_period)()
-    )
-else:
-    # Load from local file if not fetching data
-    raw_data = pd.read_csv(local_data_path, sep='\t')
-
-# Drop empty fields
-raw_data = raw_data.dropna()
-
-# Save fetched data to specified format
-if save_external_data:
-    # Determine file format based on user settings
-    file_extension = '.json' if use_json_format else '.pkl' if use_binary_format else '.csv' if use_csv_format else '.txt'
-    file_mode = 'wb' if use_binary_format else 'w'
-    with open(f'{trade_folder_path}{external_data_file_name}{file_extension}', mode=file_mode) as file:
-        if use_binary_format:
-            pickle.dump(raw_data, file)
-        elif use_json_format:
-            raw_data_json = raw_data.to_json()
-            json.dump(raw_data_json, file)
-        elif use_csv_format:  
-            file.write(str(raw_data))
-        else:
-            raw_data = pd.DataFrame(raw_data)
-            file.write(str(raw_data))
-                   
-data_list = raw_data[field].dropna().to_list()
-
-# 6. Forecasting: Calculate forecast distribution and risk
-
-# Function to be used to get the forecats of data
-FUNCTION_FORECAST = forecastData
-
-current_exchange_rate = data_list[-1]
-forecast_period = len(data_list)/3
-forecast_distribution = FUNCTION_FORECAST(data_list, current_exchange_rate, period=forecast_period)
-min_closing_rate, CLOSING_RATE, max_closing_rate = forecast_distribution
-
-TRADE_INVESTMENT = 1000 
-if ASSET_TRADING:
-    real_investment = TRADE_INVESTMENT*CLOSING_RATE*get_conversion_rate(buy_unit, currency_investment)
-else:
-    real_investment = 
-
-
-
-
-
+    output_variables[key] = f"'{val}'"
 
 output_variables.update(
 {
--- Trade_of_EUR_ZAR/EURZAR.py
+++ Trade_of_EUR_ZAR/EURZAR.py
@@ -1,7 +1,7 @@
 rate_given = "EURZAR"
-time_of_trade = '2024-10-22 14:49:10'
+time_of_trade = '2024-10-22 17:40:35'
 trade_action = "BUY ZAREUR"
-currency_investment = "ZAR"
+currency_investment = "EUR"
 currency_profit = "ZAR"
 
 FUNCTION_FORECASTtion = "forecastData"
@@ -9,19 +9,20 @@ start_date = 'None'
 end_date = 'None'
 period = '1d'
 interval = '1m'
-forecast_size = 275.66667
+forecast_size = 332.33333
 price_spread = 0.0252
 borrowing_fee = 0
+EUR_to_ZAR = 19.07
 ZAR_to_ZAR = 1.0
 initial_investment = 1000
 immediate_risk_factor = -0.00133
-sample_immediate_risk = -1.32507
+sample_immediate_risk = -25.31317
 
-forecast_closing_rates = [15.56906, 17.79318, 20.01729, 21.1827, 22.3481]
-distr_profit_factor = [-0.18134, -0.06439, 0.05256, 0.11383, 0.17511]
-sample_profit_distribution = [-181.34314, -64.39405, 52.55504, 113.83463, 175.11423]
+forecast_closing_rates = [14.74041, 17.4167, 20.09299, 21.5687, 23.04441]
+distr_profit_factor = [-0.22357, -0.08259, 0.05838, 0.13611, 0.21384]
+sample_profit_distribution = [-4263.39148, -1575.08156, 1113.22836, 2595.56747, 4077.90659]
 
-RATE_OPENING = 18.99261
-EXPECTED_CLOSING_RATE = 20.01729
-expected_sample_profit = 52.55504
-sample_max_possible_loss = -181.34314
+RATE_OPENING = 18.95954
+EXPECTED_CLOSING_RATE = 20.09299
+expected_sample_profit = 1113.22836
+sample_max_possible_loss = -4263.39148
